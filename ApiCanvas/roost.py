39c2b05bc1bf9324e0fcf9b388dbfb4d3efe28ec
stijnv18 <verheyden.stijn2@gmail.com>
2022-03-15 12:11:23 +0100

    parse rooster to array
    haalt uurooster op zet om naar array laat alle nuteloze dingen er uit en als datum zondag is haalt hij van volgende week op

--- ApiCanvas/roost.py
+++ ApiCanvas/roost.py
@@ -1,14 +1,25 @@
+
 import requests
 import datetime
 
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36"}
-cookies = {"zoneview": "false", "JSESSIONID": "826432FF7374D64C1EF6677800F4098B"}
+cookies = {"zoneview": "false", "JSESSIONID": "E0B431D91CCF800D2480F533D03745E7"}
 
 y = int(input("jaar: "))
 m = int(input("maand: "))
 d = int(input("dag: "))
-b_ts = int(datetime.datetime(y, m, d).timestamp() * 1000)
-e_ts = int(datetime.datetime(y, m, d+6, 23, 59, 59, 999999).timestamp() * 1000)
+
+ar=[]
+
+nonintb_ts=datetime.datetime(y, m, d)
+dayofweek = nonintb_ts.today().weekday()
+if dayofweek !=6:
+    nonintb_ts=nonintb_ts-datetime.timedelta(days=dayofweek)
+else:
+    nonintb_ts=nonintb_ts-datetime.timedelta(days=5-dayofweek)
+datem=datetime.datetime.strptime(str(nonintb_ts), "%Y-%m-%d %H:%M:%S")
+b_ts = int(nonintb_ts.timestamp() * 1000)
+e_ts = int(datetime.datetime(datem.year, datem.month, datem.day+6, 23, 59, 59, 999999).timestamp() * 1000)
 
 
 r = requests.get(f"https://rooster.thomasmore.be/export?format=CSV&locale=en_GB&group=false&groupByWeek=false&addSubscriptions=false&startDate={b_ts}&endDate={e_ts}", headers=headers, cookies=cookies)
@@ -17,10 +28,11 @@ r = requests.get(f"https://rooster.thomasmore.be/export?format=CSV&locale=en_GB&
 with open("rooster.CSV", "wb") as f:
     f.write(r.content)
 
+with open("rooster.CSV" ,"r") as r:
+    for line in r:
+        ar.append(line.split(";"))
 
-
-
-
-
-
-
+for i in range(2,len(ar)): 
+    print(ar[i][0])
+    print(ar[i][4])
+    print(ar[i][5])
